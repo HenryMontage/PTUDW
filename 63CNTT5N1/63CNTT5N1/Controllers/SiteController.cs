479a3a2f9dea454b551c45fc49fec88424a3ffdd
Kiều Thái Tuấn <thaichualam123@gmail.com>
2023-09-30 10:52:35 +0700

    Buoi2_finish

--- 63CNTT5N1/63CNTT5N1/Controllers/SiteController.cs
+++ 63CNTT5N1/63CNTT5N1/Controllers/SiteController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyClass.Model;
 
 namespace _63CNTT5N1.Controllers
 {
@@ -13,5 +14,7 @@ namespace _63CNTT5N1.Controllers
         {
             return View();
         }
+
     }
+
 }
